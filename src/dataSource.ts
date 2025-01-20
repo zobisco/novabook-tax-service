c7cc6cde7c4cff34785559041839b26de3e425ca
zobisco <zoebscott@hotmail.com>
2025-01-20 11:32:26 +0000

    chore: update dataSource with new entity name

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,13 +1,13 @@
 import { DataSource } from 'typeorm';
 import { TransactionEntity } from './database/entities/transaction.entity';
-import { SaleAmendmentEntity } from './database/entities/saleAmendment.entity';
+import { AmendSaleEntity } from './database/entities/amendSale.entity';
 import { SaleItemEntity } from './database/entities/saleItem.entity';
 
 const dataSource = new DataSource({
-	type: 'sqlite',
-	database: 'novabook.sqlite',
-	entities: [SaleAmendmentEntity, SaleItemEntity, TransactionEntity],
-	migrations: ['./dist/src/database/migrations/*.js'],
+  type: 'sqlite',
+  database: 'novabook.sqlite',
+  entities: [AmendSaleEntity, SaleItemEntity, TransactionEntity],
+  migrations: ['./dist/src/database/migrations/*.js'],
 });
 
 export default dataSource;
